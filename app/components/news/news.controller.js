8b2c81cd25c7199b5554e4f9cd40b19a0357c5eb
Vyachaslav Kuzharov <marhuz.61@gmail.com>
2016-05-27 17:28:16 +0300

    Add main page-proofs

--- app/components/news/news.controller.js
+++ app/components/news/news.controller.js
@@ -4,12 +4,19 @@
     angular.module('ngTagNews')
         .controller('newsController', newsController);
 
-    // newsController.$inject = []
+    newsController.$inject = ['$stateParams'];
 
-    function newsController() {
+    function newsController($stateParams) {
         var vm = this;
-        
-        vm.message = 'Bootstrap works?'
+        vm.pageChanged = pageChanged;
+
+        vm.maxSize = 5;
+        vm.CurrentPage = $stateParams.page || 1;
+        vm.totalItems = 50;
+
+        function pageChanged() {
+
+        }
 
     }
 })();
